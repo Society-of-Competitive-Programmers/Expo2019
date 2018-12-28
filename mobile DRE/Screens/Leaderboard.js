b3e90b9c168db0d4bb2197f5e33bd74e563c89d0
mjsumpter <mjsumpter@gmail.com>
2018-12-27 23:15:41 -0500

    generating dummy values for leaderboard

--- mobile DRE/Screens/Leaderboard.js
+++ mobile DRE/Screens/Leaderboard.js
@@ -3,10 +3,16 @@ import { StyleSheet, Text, View } from 'react-native';
 
 export default class Leaderboard extends React.Component {
   
+  constructor(props) {
+    super();
+    this.topTen = populateLeaderboard(10);
+  }
+
   render() {
     return (
       <View style={styles.container}>
-   
+        <Text
+          style={{ marginBottom: 20, marginTop: 20, fontSize: 30 }}> D.R.E. Leaderboard </Text>
       </View>
       
     );
@@ -21,3 +27,17 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
 });
+
+function populateLeaderboard(numLeaders) {
+  let leaderArray = [];
+  for(let i = 0; i < numLeaders; i++)
+  {
+    let user = {
+      name: `Player${i}`,
+      age: i + 10,
+      school: 'USF'
+    };
+    leaderArray.push(user);
+  }
+  return leaderArray;
+}
