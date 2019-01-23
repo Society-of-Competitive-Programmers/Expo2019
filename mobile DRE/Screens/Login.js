c2f9c0dc6716ec805957b24c6ef837598dcf8cb7
mjsumpter <mjsumpter@gmail.com>
2019-01-22 20:51:56 -0500

    created textInput style rule in Login screen

--- mobile DRE/Screens/Login.js
+++ mobile DRE/Screens/Login.js
@@ -1,21 +1,29 @@
-/* 
-*  Created by Jim Mirzakhalov, October 25, 2018
-*   
-*  To be able to run the app, run "npm install firebase --save"
-*/
-import React from 'react';
-import { StyleSheet, Text, View, TextInput, Button, Image, ScrollView} from 'react-native';
-import { KeyboardAwareScrollView}  from 'react-native-keyboard-aware-scroll-view'
-import * as firebase from 'firebase';
+/*
+ *  Created by Jim Mirzakhalov, October 25, 2018
+ *
+ *  To be able to run the app, run "npm install firebase --save"
+ */
+import React from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  TextInput,
+  Button,
+  Image,
+  ScrollView
+} from "react-native";
+import { KeyboardAwareScrollView } from "react-native-keyboard-aware-scroll-view";
+import * as firebase from "firebase";
 
 // firebase configurations for the project
 const firebaseConfig = {
   apiKey: "AIzaSyCyAOjCgRO1zjYhbEM-_si8Mgb6WHoCep8",
-    authDomain: "scp-dre.firebaseapp.com",
-    databaseURL: "https://scp-dre.firebaseio.com",
-    projectId: "scp-dre",
-    storageBucket: "",
-    messagingSenderId: "758043467370"
+  authDomain: "scp-dre.firebaseapp.com",
+  databaseURL: "https://scp-dre.firebaseio.com",
+  projectId: "scp-dre",
+  storageBucket: "",
+  messagingSenderId: "758043467370"
 };
 
 // initializing the firebase instance here
@@ -25,8 +33,6 @@ firebase.initializeApp(firebaseConfig);
 const database = firebase.database();
 
 export default class Login extends React.Component {
-  
-
   constructor() {
     super();
     this.state = {
@@ -42,104 +48,102 @@ export default class Login extends React.Component {
 
       age: "",
 
-      school: "",
-    }
+      school: ""
+    };
 
     this._navigate.bind(this);
-
   }
 
   componentDidMount() {
-    Expo.ScreenOrientation.allow(Expo.ScreenOrientation.Orientation.ALL_BUT_UPSIDE_DOWN);
+    Expo.ScreenOrientation.allow(
+      Expo.ScreenOrientation.Orientation.ALL_BUT_UPSIDE_DOWN
+    );
   }
 
   // Firebase Team:
-  
+
   // make a json object of the user, and push it to the database. User 'push' instead of 'set'. Then call _navigate to move to the next page
-  _registerUser(){
-   
+  _registerUser() {
     const profile = {
       name: this.state.name,
       age: this.state.age,
-      school: this.state.school,
-    }
-    
-    if(profile.name == "" || profile.age == "" || profile.school == ""){
-        alert("All fields must be filled in");
-    }
-    else{
+      school: this.state.school
+    };
+
+    if (profile.name == "" || profile.age == "" || profile.school == "") {
+      alert("All fields must be filled in");
+    } else {
       database.ref("users/").push(profile);
-      this.props.navigation.navigate('Stream');
+      this.props.navigation.navigate("Stream");
     }
   }
 
-  // DO NOT WRITE ANY CODE IN HERE YET // 
-  _navigate(){
-   
+  // DO NOT WRITE ANY CODE IN HERE YET //
+  _navigate() {
     const { navigate } = this.props.navigation;
     navigate("Stream");
   }
-  
-  render() {
 
+  render() {
     return (
       // Login Page Layout Team:
       // put all the layout elements inside this view i.e. all the text input boxes, buttons, all the styles
       //TODO
-      <ScrollView style={{width: "100%"}}>
-      <KeyboardAwareScrollView 
-        style={{paddingTop: 100}}
-        resetScrollToCoords={{ x: 0, y: 0 }}
-        contentContainerStyle={styles.container}
-        scrollEnabled={true}
-        animated={true}
-      >
-      
-        <Image
-          source={{
-            uri: 'https://facebook.github.io/react/logo-og.png',
-            // cache: 'only-if-cached',
-          }}
-          style={{width: 200, height: 200}}
-        />
-        <Text
-         style = {{marginBottom: 20, marginTop: 20,  fontSize: 30}}> D.R.E. </Text>
-        <TextInput
-          style={{height: 40, width: 250, borderColor: 'gray', backgroundColor: '#D3D3D3', borderWidth: 1, marginBottom: 10, paddingLeft: 5}}
-          onChangeText={(name) => this.setState({name})}
-          value={this.state.name}
-          placeholder = {"Name"}
-          placeholderTextColor = "black"
-          windowSoftInputMode = "adjustResize"
-        />
-        <TextInput
-          style={{height: 40, width: 250, borderColor: 'gray', backgroundColor: '#D3D3D3',  borderWidth: 1, marginBottom: 10, paddingLeft: 5}}
-          onChangeText={(age) => this.setState({age})}
-          value={this.state.age}
-          placeholder = {"Age"}
-          placeholderTextColor = "black"
-          windowSoftInputMode = "adjustResize"
-        />
-        <TextInput
-          style={{height: 40, width: 250, borderColor: 'gray', backgroundColor: '#D3D3D3', borderWidth: 1, marginBottom: 20, paddingLeft: 5}}
-          onChangeText={(school) => this.setState({school})}
-          value={this.state.school}
-          placeholder = {"School"}
-          placeholderTextColor = "black"
-          windowSoftInputMode = "adjustResize"
-        />
+      <ScrollView style={{ width: "100%" }}>
+        <KeyboardAwareScrollView
+          style={{ paddingTop: 100 }}
+          resetScrollToCoords={{ x: 0, y: 0 }}
+          contentContainerStyle={styles.container}
+          scrollEnabled={true}
+          animated={true}
+        >
+          {/* <Image
+            source={{
+              uri: "https://facebook.github.io/react/logo-og.png"
+              // cache: 'only-if-cached',
+            }}
+            style={{ width: 200, height: 200 }}
+          /> */}
+          <Text style={{ marginBottom: 20, marginTop: 20, fontSize: 30 }}>
+            {" "}
+            D.R.E.{" "}
+          </Text>
+          <TextInput
+            style={styles.textInput}
+            onChangeText={name => this.setState({ name })}
+            value={this.state.name}
+            placeholder={"Name"}
+            placeholderTextColor="black"
+            windowSoftInputMode="adjustResize"
+          />
+          <TextInput
+            style={styles.textInput}
+            onChangeText={age => this.setState({ age })}
+            value={this.state.age}
+            placeholder={"Age"}
+            placeholderTextColor="black"
+            windowSoftInputMode="adjustResize"
+          />
+          <TextInput
+            style={styles.textInput}
+            onChangeText={school => this.setState({ school })}
+            value={this.state.school}
+            placeholder={"School"}
+            placeholderTextColor="black"
+            windowSoftInputMode="adjustResize"
+          />
 
-        <Button
-          onPress={this._registerUser.bind(this)/*this._registerUser()*/}
-          title="Let's Get Started!!!"
-          color="#0066ff"
-        />
-        <Button
-          onPress={() => this.props.navigation.navigate('Leaderboard')}
-          title="Leaderboard"
-          color="#ff0000"
-        />
-      </KeyboardAwareScrollView>
+          <Button
+            onPress={this._registerUser.bind(this) /*this._registerUser()*/}
+            title="Let's Get Started!!!"
+            color="#0066ff"
+          />
+          <Button
+            onPress={() => this.props.navigation.navigate("Leaderboard")}
+            title="Leaderboard"
+            color="#ff0000"
+          />
+        </KeyboardAwareScrollView>
       </ScrollView>
       //TODO
     );
@@ -149,8 +153,17 @@ export default class Login extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center"
   },
+  textInput: {
+    height: 40,
+    width: 250,
+    borderColor: "gray",
+    backgroundColor: "#D3D3D3",
+    borderWidth: 1,
+    marginBottom: 10,
+    paddingLeft: 5
+  }
 });
