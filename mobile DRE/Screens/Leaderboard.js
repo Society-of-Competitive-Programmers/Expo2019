9fe65ad117816aafe3f8a4997b2211053b579ff2
mjsumpter <mjsumpter@gmail.com>
2019-01-24 11:14:33 -0500

    Leaderboard - GUI styling full update

--- mobile DRE/Screens/Leaderboard.js
+++ mobile DRE/Screens/Leaderboard.js
@@ -1,12 +1,6 @@
 import React from "react";
 import { StyleSheet, Text, View, ScrollView, Button } from "react-native";
-import {
-  Table,
-  TableWrapper,
-  Row,
-  Rows,
-  Col
-} from "react-native-table-component";
+import { Table, TableWrapper, Row, Cell } from "react-native-table-component";
 import * as firebase from "firebase";
 
 const database = firebase.database();
@@ -16,9 +10,8 @@ export default class Leaderboard extends React.Component {
     super(props);
 
     this.state = {
-      numLeaders: 0,
+      widthArr: [1, 3, 1, 3, 2], //width of columns
       tableHead: ["", "Name", "Age", "School", "Time"],
-      tableTitle: [],
       topPlayers: [],
       tableData: []
     };
@@ -29,21 +22,17 @@ export default class Leaderboard extends React.Component {
   // Receives user data, sorts it, and populates table state
   onUserData(users) {
     let leaderArray = [];
-    let numUsers = 0;
-    let titleArray = [];
+
     users.forEach(usersChild => {
-      let player = usersChild.val();
+      let player = usersChild.val(); // player object
       leaderArray.push(player);
-      titleArray.push(++numUsers);
     });
-    let playerArray = populateLeaderboard(leaderArray, this.numLeaders);
+    let playerArray = populateLeaderboard(leaderArray);
     let dataArray = populateTableData(playerArray);
 
     this.setState({
       topPlayers: playerArray,
-      tableData: dataArray,
-      numLeaders: numUsers,
-      tableTitle: titleArray
+      tableData: dataArray
     });
   }
 
@@ -58,12 +47,19 @@ export default class Leaderboard extends React.Component {
 
   render() {
     const state = this.state;
+    // rankElement is yellow circle that displays rank
+    const rankElement = data => (
+      <View style={styles.rank}>
+        <Text style={styles.text}>{data}</Text>
+      </View>
+    );
 
     return (
       <View style={styles.container}>
         <Text style={styles.header}>D.R.E. Leaderboard</Text>
         <ScrollView
           style={{ width: "100%" }}
+          contentContainerStyle={{ alignItems: "center" }}
           showsVerticalScrollIndicator={false}
         >
           <ScrollView
@@ -71,36 +67,45 @@ export default class Leaderboard extends React.Component {
             showsHorizontalScrollIndicator={false}
             directionalLockEnabled={false}
           >
-            <Table style={styles.table}>
+            <Table
+              style={styles.table}
+              borderStyle={{ borderColor: "transparent" }} // removes table lines
+            >
               <Row
                 data={state.tableHead}
-                flexArr={[1, 1, 1, 1, 1]}
+                flexArr={state.widthArr}
                 style={styles.head}
                 textStyle={styles.text}
               />
-              <TableWrapper style={styles.wrapper}>
-                <Col
-                  data={state.tableTitle}
-                  style={styles.title}
-                  heightArr={[28, 28]}
-                  textStyle={styles.text}
-                />
-                <Rows
-                  data={state.tableData}
-                  flexArr={[1, 1, 1, 1]}
-                  style={styles.row}
-                  textStyle={styles.text}
-                />
-              </TableWrapper>
+              {/*create table wrapper for each row */}
+              {state.tableData.map((rowData, index) => (
+                <TableWrapper key={index} style={styles.row}>
+                  {rowData.map((cellData, cellIndex) => (
+                    <Cell
+                      key={cellIndex}
+                      data={cellIndex === 0 ? rankElement(cellData) : cellData} //creates circle element if first cell
+                      style={[
+                        {
+                          width: state.widthArr[cellIndex] * 40, //cell width approx. proportional to widthArr
+                          height: 30
+                        },
+                        index % 2 && { backgroundColor: "#F7F6E7" } //varies color of cell depending on which row its in
+                      ]}
+                      textStyle={styles.tableText}
+                    />
+                  ))}
+                </TableWrapper>
+              ))}
             </Table>
           </ScrollView>
         </ScrollView>
-
-        <Button
-          onPress={() => this.props.navigation.navigate("Login")}
-          title="Back to Login Screen"
-          color="#ff0000"
-        />
+        <View style={{ width: 380, alignSelf: "center" }}>
+          <Button
+            onPress={() => this.props.navigation.navigate("Login")}
+            title="Back to Login Screen"
+            color="#18CD12"
+          />
+        </View>
       </View>
     );
   }
@@ -111,7 +116,8 @@ const styles = StyleSheet.create({
     flex: 1,
     padding: 16,
     paddingTop: 30,
-    backgroundColor: "#23604E"
+    backgroundColor: "#23604E",
+    width: "100%"
   },
   header: {
     fontSize: 20,
@@ -122,32 +128,41 @@ const styles = StyleSheet.create({
     color: "#fff"
   },
   table: { width: 400 },
-  head: { height: 40, backgroundColor: "#000000" },
+  head: { height: 40, backgroundColor: "#000" },
   wrapper: { flexDirection: "row" },
   title: { flex: 1, backgroundColor: "#ECD459" },
-  row: { height: 28 },
-  text: { textAlign: "center", color: "#fff" }
+  row: { height: 28, backgroundColor: "#E7E6E1", flexDirection: "row" },
+  text: { textAlign: "center", color: "#fff" },
+  tableText: { textAlign: "center", color: "#000" },
+  rank: {
+    borderRadius: 50,
+    backgroundColor: "#ECD459",
+    width: 30,
+    alignSelf: "center"
+  }
 });
 
 //sorts players by determined property and returns the desired number of results
-function populateLeaderboard(leaderArray, numLeaders) {
+function populateLeaderboard(leaderArray) {
   //filter out empty name fields
-  // leaderArray = leaderArray.filter(player => player.name !== "");
-  //sort array by name
+  leaderArray = leaderArray.filter(player => player.name !== "");
+  //sort array by score
   leaderArray.sort((a, b) => {
-    if (a.name < b.name) return -1;
-    if (a.name > b.name) return 1;
+    if (a.score > b.score) return -1;
+    if (a.score < b.score) return 1;
     return 0;
   });
   //return only desired top number of players
-  return leaderArray.slice(0, numLeaders);
+  return leaderArray;
 }
 
 // populates table data with player info
 function populateTableData(topPlayers) {
   let tableData = [];
+  let i = 1;
   topPlayers.forEach(player => {
     let rowData = [];
+    rowData.push(i++); // player leaderboard position
     rowData.push(player.name);
     rowData.push(player.age);
     rowData.push(player.school);
